dbe91268a67c0d76d29c20789e04cf9bbafed884
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-11-17 01:45:39 +0200

    task 0

---
+++ pall.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+*pall - print the stack elements
+*@line: it not be used , we use the attribute not void
+*@s: pointer to head of a doubly linked list
+*Return: void
+*/
+
+void pall(stack_t **s, unsigned int __attribute__((unused)) line)
+{
+    stack_t *current = *s;
+
+    if (s == NULL || *s == NULL)
+    return;
+
+    while (current != NULL)
+    {
+    printf("%d\n", current->n);
+    current = current->next;
+    }
+}
---
+++ push.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+
+
+/**
+ * push - add the node to stack
+ * @head: it the stack head
+ * @n: it number
+ * @counter: it the line_number
+ * Return: void
+*/
+
+
+void push(stack_t **head, char *n, unsigned int counter)
+{
+	stack_t *new = NULL;
+	int i;
+
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (n == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 0; n[i]; i++)
+	{
+		if (n[0] == '-' && i == 0)
+			continue;
+		if (n[i] < 48 || n[i] > 57)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	new->n = atoi(n);
+	new->prev = NULL;
+	new->next = NULL;
+
+	if (*head != NULL)
+	{
+		new->next = *head;
+		(*head)->prev = new;
+	}
+	*head = new;
+}
